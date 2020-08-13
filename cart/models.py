24c1a18f64065f9f33eeb1d72e8052fb892a1be1
hlwarun <hlwarun@gmail.com>
2020-08-13 18:23:31 +0545

    Cart

--- cart/models.py
+++ cart/models.py
@@ -5,6 +5,7 @@ from shop.models import Product
 
 class Cart(models.Model):
     products = models.ManyToManyField(Product, blank=True)
+    subtotal_price = models.DecimalField(max_digits=10, decimal_places=2)
     total_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
--- cart/views.py
+++ cart/views.py
@@ -20,4 +20,15 @@ def update_cart(request, slug):
         carts.products.add(product)
     else:
         carts.products.remove(product)
+
+    sub_total = 0.00
+    total = 0.00
+    tax = 0.13
+    shipping = 100 # Per item
+    for item in carts.products.all():
+        sub_total += float(item.new_price)
+        total += (float(item.new_price))*(1 + tax) + shipping
+    carts.subtotal_price = sub_total
+    carts.total_price = total
+    carts.save()
     return HttpResponseRedirect(reverse('cart:cart'))
