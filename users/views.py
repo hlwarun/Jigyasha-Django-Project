509768e34e7cb0604991d8606f39043f82bd1316
hlwarun <hlwarun@gmail.com>
2020-08-15 23:52:09 +0545

    Login Functionality

--- users/views.py
+++ users/views.py
@@ -24,10 +24,25 @@ def register(request):
                 user = User.objects.create_user(username=username,email=email,password=password,first_name=first_name,last_name=last_name)
                 user.save()
                 messages.success(request, f'Congratulations! {first_name}, Your account has been created successfully!')
-                return redirect('root:home')
+                return redirect('users:login')
         else:
             messages.info(request, 'Password do not match.')
             return redirect('users:register')
 
     else:
         return render(request, 'signup.html')
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('root:home')
+        else:
+            messages.error(request, 'Sorry! either username or Password do not match!')
+            return redirect('users:login')
+    else:
+        return render(request, 'login.html')
